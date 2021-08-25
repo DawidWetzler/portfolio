b2b91e8edd1099dbaf1bc5a15af37a18cb09e171
DawidWetzler <wetzlerdawid@gmail.com>
2021-08-25 17:47:49 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    index page
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+
+@Component
+export default class index extends Vue {
+  //
+}
+</script>
