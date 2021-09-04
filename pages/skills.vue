b9945e4eda11a90856580c8867748dece248072f
DawidWetzler <wetzlerdawid@gmail.com>
2021-09-04 14:04:11 +0200

    make skills page prettier

--- pages/skills.vue
+++ pages/skills.vue
@@ -1,26 +1,32 @@
 <template>
   <main class="flex-1 flex justify-center items-center max-w-7xl w-full mx-auto">
-    <ul class="grid grid-cols-2 md:grid-cols-3 grid-rows-2 gap-2">
-      <li
-        v-for="skill in skills"
-        :key="`skills-${skill.title}`"
-        class="text-white bg-gray-800 shadow px-4 py-2 rounded-lg"
-      >
-        <span class="text-base md:text-xl tracking-tight text-montserrat text-white">{{ skill.title }}</span>
-        <ul
-          v-if="skill.skills.length >= 1"
-          class="list-disc mt-1"
+    <div class="max-w-4xl w-full bg-gray-800 bg-opacity-80 shadow rounded-lg p-6 space-y-8">
+      <h1 class="text-center text-white tracking-tight text-montserrat text-3xl lg:text-5xl mt-4">
+        My <span class="text-lime">skills</span>
+      </h1>
+
+      <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-2 gap-4">
+        <li
+          v-for="skill in skills"
+          :key="`skills-${skill.title}`"
+          class="text-white bg-gray-800 bg-opacity-80 shadow p-6 rounded-lg"
         >
-          <li
-            v-for="skill in skill.skills"
-            :key="`skill-${skill}`"
-            class="text-sm md:text-base ml-5 text-montserrat text-gray-300"
+          <span class="text-base md:text-xl tracking-tight text-montserrat text-white">{{ skill.title }}</span>
+          <ul
+            v-if="skill.skills.length >= 1"
+            class="list-outside list-disc mt-1"
           >
-            {{ skill }}
-          </li>
-        </ul>
-      </li>
-    </ul>
+            <li
+              v-for="skill in skill.skills"
+              :key="`skill-${skill}`"
+              class="text-sm md:text-base ml-5 text-montserrat text-lime"
+            >
+              <span class="text-gray-300">{{ skill }}</span>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </main>
 </template>
 
@@ -36,7 +42,7 @@ export default class Skills extends Vue {
         skills: ['TailwindCSS', 'Native', 'Bootstrap', 'SCSS / LESS'],
       },
       {
-        title: 'Java & Typescript',
+        title: 'Javascript & Typescript',
         skills: ['JQuery', 'VueJS / NuxtJS', 'React / NextJS', 'Native']
       },
       {
