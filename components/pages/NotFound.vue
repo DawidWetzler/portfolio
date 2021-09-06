98e67012527cf17049eda73fa9effcef419172c0
DawidWetzler <wetzlerdawid@gmail.com>
2021-09-06 22:05:08 +0200

    error pages added

---
+++ components/pages/NotFound.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex items-center">
+    <div>
+      <p class="text-white text-3xl lg:text-5xl font-medium mt-8 mb-10">Page not found</p>
+      <p class="text-gray-300">The page you we're looking for is not available.</p>
+      <nuxt-link
+        class="flex px-6 py-4 border-2 border-gray-100 w-52 mt-10 md:hover:bg-gray-900 md:hover:border-gray-300 hover:text-white transition duration-200 ease-in-out"
+        to="/"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" class="text-white h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16l-4-4m0 0l4-4m-4 4h18" />
+        </svg>
+        <span class="text-white ml-3">Take me home</span>
+      </nuxt-link>
+    </div>
+    <div>
+      <img class="h-32 md:h-64 2xl:h-96 mt-12 md:mt-0 md:ml-12" src="~/assets/images/404.svg" alt="404 error" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component
+export default class NotFound extends Vue {
+  //
+}
+</script>
---
+++ layouts/error.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="flex-1 flex justify-center items-center max-w-7xl w-full mx-auto">
+
+    <NotFound v-if="error.statusCode === 404" />
+    <FatalError v-else />
+
+  </main>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator';
+import NotFound from '@/components/pages/NotFound.vue';
+import FatalError from '@/components/pages/FatalError.vue';
+
+@Component({
+  components: { NotFound, FatalError }
+})
+export default class Error extends Vue {
+  @Prop({ default: () => ({})}) readonly error!: { statusCode: number; message: string };
+}
+</script>
+
